fc1a0b1c9ddc0ca5dd638737a6c55dd21817c614
AdrianHernandezll <danix08@live.com>
2021-04-30 00:06:34 -0300

    funcion modo noche y clases

---
+++ js/style-switcher.js
@@ -0,0 +1,48 @@
+/** =================Toggle estlilos del Swtich================= */
+
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+
+styleSwitcherToggler.addEventListener("click", () =>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+
+})
+
+// Estilo Hide - Switch en Scroll
+window.addEventListener("scroll", () =>{
+     if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector(".style-switcher").classList.remove("open");
+     }
+})
+
+/* ======================== COLOR TEMAS==================== */
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color){
+    alternateStyles.forEach((style) =>{
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disable");
+        }
+        else{
+            style.setAttribute("disable","true");
+        }
+    })
+
+}
+
+
+/**==============TEMA BLANCO Y OSCURO============ */
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () =>{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+
+window.addEventListener("load", () =>{
+    if(document.body.classList.contains("dark")){
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon")
+    }
+})
